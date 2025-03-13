ccd4d2be5e71d6fa00dd13685885341e89c0f2e3
annmariet <annmariethomson.1@gmail.com>
2025-03-12 21:25:40 -0700

    Updated script 3, not yet script 4

---
+++ scripts/03_visualizations.R
@@ -0,0 +1,61 @@
+library(tidyverse)
+library(tidymodels)
+library(ggplot2)
+library(ggpubr)
+library(corrplot)
+
+bike_data <- read.csv("data/cleaned/bike_data.csv")
+
+## Exploratory data analysis
+
+temp_vs_rentals = ggplot(bike_data, aes(x = temp, y = cnt)) +
+  geom_point(color = "blue", alpha = 0.5) +
+  theme_minimal() +
+  labs(title = "Temperature vs Bike Rentals", x = "Temperature", y = "Total Bike Rentals")
+
+
+weather_vs_cnt = ggplot(bike_data, aes(x = factor(weathersit), y = cnt, fill = factor(weathersit))) +
+  geom_bar(stat = "identity") +
+  scale_x_discrete(labels=c("Clear/Partly Cloudy", "Mist and Cloudy", "Light Percipitation", "Heavy Percipitation")) +
+  labs(title = "Bike Rentals by \nWeather Situation", x = "Weather Situation", y = "Total Bike Rentals") +
+  theme(axis.text.x = element_text(angle = 70, vjust = 0.5))
+
+
+season_vs_cnt = ggplot(bike_data, aes(x = factor(season), y = cnt)) +
+  geom_boxplot(fill = "lightgreen", color = "black", alpha = 0.7) +
+  labs(title = "Bike Rentals by Season", x = "Season", y = "Total Bike Rentals")
+
+weekday_rental = ggplot(bike_data, aes(x = factor(weekday), y = cnt, fill = factor(weekday))) +
+  geom_bar(stat = "identity", show.legend = FALSE) +
+  scale_fill_viridis_d() +
+  scale_x_discrete(labels=c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")) +
+  labs(title = "Bike Rentals by Weekday", x = "Weekday", y = "Total Bike Rentals") +
+  theme(axis.text.x = element_text(angle = 70, vjust = 0.5))
+
+humidty_rental = ggplot(bike_data, aes(x = hum, y = cnt)) +
+  geom_point(color = "purple", alpha = 0.5) +
+  labs(title = "Humidity vs Bike Rentals", x = "Humidity", y = "Total Bike Rentals") +
+  theme(axis.text.x = element_text(angle = 70, vjust = 0.5))
+
+wind_rental = ggplot(bike_data, aes(x = windspeed, y = cnt)) +
+  geom_point(color = "green", alpha = 0.5) +
+  theme_minimal() +
+  labs(title = "Windspeed vs Bike Rentals", x = "Wind Speed", y = "Total Bike Rentals")
+
+
+exploratory = ggarrange(temp_vs_rentals, weather_vs_cnt, season_vs_cnt, weekday_rental, humidty_rental, wind_rental)
+
+show(exploratory)
+ggsave("output/exploratory.png", plot=exploratory)
+
+total_rentals = ggplot(bike_data,aes(x = cnt))+
+geom_histogram(binwidth = 10, fill = "blue",color = "black",alpha = 0.7)+
+labs(title = "Histogram of the total bike rentals", x = "Total rentals")
+
+show(total_rentals)
+ggsave("output/total.png", plot=total_rentals)
+
+png(file = "output/cor_plot.png")
+cor = cor(bike_data %>% select(temp, atemp, hum, windspeed, casual, registered, cnt))
+cor_plot = corrplot(cor)
+dev.off()
