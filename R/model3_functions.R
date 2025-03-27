e68c26f134ae6b40a52e818bdd24b0075ca6cdf5
Arya-Sardesai <aryasardesai6@gmail.com>
2025-03-27 16:55:55 -0700

    Arya:updated model3_functions file with splitting data and fitting model functions

---
+++ R/model3_functions.R
@@ -0,0 +1,12 @@
+# splitting data
+split_data <- function(data,prop = 0.75, strata = 'cnt'){
+    set.seed(2024)
+    split <- initial_split(data, prop = 0.75,strata = 
+    all_of(strata))
+    list(train = training(split), test = testing(split))
+}
+
+fit_best_model <- function(train_data) {
+    lm(log(cnt) ~ season+ temp + weathersit, 
+    data = train_data)
+}
