659cdb10f924fb9306d9168bdc60dd167d76ee90
Christina Zhang <christinazhang2671@gmail.com>
2025-03-28 02:20:05 -0700

    Add cleaning function, test file, and script refactor

--- scripts/02_clean_data.R
+++ scripts/02_clean_data.R
@@ -1,20 +1,20 @@
 library(tidyverse)
 library(docopt)
+library(here)
+
+setwd("/Users/ck/DSCI310-2024W2/dsci-310-group-16")
+source("R/cleaning.R")
 
 "This script loads, cleans, saves titanic data
 
 Usage: 02_clean_data.R --file_path=<file_path> --output_path=<output_path>
 " -> doc
 
+here::i_am("scripts/02_clean_data.R")
+source(here::here("R", "cleaning.R"))
+
 opt <- docopt(doc)
 
-bike_data <- read.csv(opt$file_path)
-bike_data <- bike_data %>%
-  select(-instant, -dteday)
-bike_data <- bike_data %>%
-  mutate(weathersit = as.factor(weathersit))
-bike_data <- bike_data %>%
-  mutate(cnt = as.numeric(cnt))
-bike_data <- bike_data %>%
-  rename(id = X)
-write_csv(bike_data, opt$output_path)
+bike_data <- read_csv(opt$file_path)
+bike_data_cleaned <- clean_bike_data(bike_data)
+write_csv(bike_data_cleaned, opt$output_path)
---
+++ test/testthat/test_cleaning.R
@@ -0,0 +1,26 @@
+library(testthat)
+library(dplyr)
+library(readr)
+
+source("R/cleaning.R")
+
+test_that("clean_bike_data removes and renames columns, and converts types", {
+    sample_data <- tibble::tibble(
+        `...1` = 1:2,
+        instant = 1:2,
+        dteday = c("2021-01-01", "2021-01-02"),
+        weathersit = c(1, 2),
+        cnt = c("100", "200")
+    )
+        
+    cleaned <- clean_bike_data(sample_data)
+    
+    # Check column name
+    expect_false("instant" %in% colnames(cleaned))
+    expect_false("dteday" %in% colnames(cleaned))
+    expect_true("id" %in% colnames(cleaned))
+
+    # Check types
+    expect_s3_class(cleaned$weathersit, "factor")
+    expect_type(cleaned$cnt, "double")
+})
